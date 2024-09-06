45ffbaff42894a363dff4ca2c937978ffc0c97ae
Jing Wei <jingweichan97@gmail.com>
2024-09-07 00:27:26 +0800

    fix: change file location

--- src/utils/fsHandle.ts
+++ src/utils/fsHandle.ts
@@ -1,13 +1,13 @@
 import FS from 'fs';
 
 export function retrieve() {
-    if (!FS.existsSync('./src/file.json')) {
+    if (!FS.existsSync('./file.json')) {
         return [];
     }
-    const jsonData = require('../file.json');
+    const jsonData = require('../../file.json');
     return jsonData.events;
 }
 
 export function writeToFile(events) {
-    FS.promises.writeFile('./src/file.json', JSON.stringify({ events }, null, 2));
+    FS.promises.writeFile('./file.json', JSON.stringify({ events }, null, 2));
 }
